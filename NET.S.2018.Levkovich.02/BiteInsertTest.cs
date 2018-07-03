3c9b0780532f87723cdf6287f136bd709b3f4161
Anastacia Levkovich <levkovich.astya@gmail.com>
2018-07-04 00:08:22 +0300

    first

---
+++ NET.S.2018.Levkovich.02/BiteInsertTest.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using static _1.BiteInsert;
+
+namespace _1.Test
+{
+    [TestClass]
+    public class BiteInsertTest
+    {
+       
+        [TestMethod]
+        public void InsertWithShift()
+        {
+            // Arrange
+            int expected = InsertNumber(8, 15, 3, 8);
+
+            // Act
+            int actual = 120;
+
+            // Assert
+            Assert.AreEqual(expected, actual);
+        }
+
+        [TestMethod]
+        public void InsertWithoutShift()
+        {
+            // Arrange
+            int expected = InsertNumber(15, 15, 0, 0);
+
+            // Act
+            int actual = 15;
+
+            // Assert
+            Assert.AreEqual(expected, actual);
+        }
+        [TestMethod]
+        public void InsertWithoutShiftAndWithDiffNumber()
+        {
+            // Arrange
+            int expected = InsertNumber(8, 15, 0, 0);
+
+            // Act
+            int actual = 9;
+
+            // Assert
+            Assert.AreEqual(expected, actual);
+        }
+        [TestMethod]
+        public void InsertWithNegativeNumber()
+        {
+            // Arrange
+            int expected = InsertNumber(-16, 19, 2, 6);
+
+            // Act
+            int actual = -4;
+
+            // Assert
+            Assert.AreEqual(expected, actual);
+        }
+        [TestMethod]
+        [ExpectedException(typeof(ArgumentOutOfRangeException))]
+        public void InsertNumber_IndexMoreMaxLength_Exception()
+                => InsertNumber(15, 15, 0, 80);
+
+        [TestMethod]
+        [ExpectedException(typeof(ArgumentOutOfRangeException))]
+        public void InsertNumber_NegativeIndex_Exception()
+                => InsertNumber(15, 15, -7, 33);
+
+        [TestMethod]
+        [ExpectedException(typeof(ArgumentException))]
+        public void InsertNumber_jLessThani_Exception()
+                => InsertNumber(1, 1, 5, 0);
+    }
+}
---
+++ NET.S.2018.Levkovich.02/FilterTest.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using static _6.Filter;
+    
+
+namespace _6.Test
+{
+    [TestClass]
+    public class FilterTest
+    {
+        [TestMethod]
+        public void FilterGigit_Array_FiltedArray()
+        {
+            // Arrange
+            int[] numeralList = { 7, 1, 2, 3, 4, 5, 6, 7, 68, 69, 70, 15, 17 };
+            int number = 7;
+            int[] expected = { 7, 7, 70, 17 };
+
+            // Act
+            FilterDigit(ref numeralList, number);
+
+            // Assert
+            CollectionAssert.AreEqual(expected, numeralList);
+        }
+        [TestMethod]
+        public void FilterDigit_ArrayWithout_EmptyArray()
+        {
+            int[] numeralList = { 1, 2, 3, 5, 6, 68, 69, 15 };
+            int number = 7;
+            int[] expected = { };
+
+            FilterDigit(ref numeralList, number);
+
+            CollectionAssert.AreEqual(expected, numeralList);
+        }
+        //[TestMethod]
+        //[ExpectedException(typeof(ArgumentNullException))]
+        //public void FilterGigit_InputWithNull_ThrowArgumentNullException()
+        //       =>FilterDigit(ref null, 7);
+    }
+}
