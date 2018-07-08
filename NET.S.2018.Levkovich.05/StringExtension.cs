b4cf3cda4748db175589ceb02c21c2a1ea275c5a
Anastacia Levkovich <levkovich.astya@gmail.com>
2018-07-08 17:04:50 +0300

    String Extension

---
+++ NET.S.2018.Levkovich.05/StringExtension.cs
@@ -0,0 +1,59 @@
+﻿using System;
+
+namespace ConvertToDecimal
+{
+    /// <summary>
+    /// extention 
+    /// </summary>
+    public static class StringExtension
+    {
+        /// <summary>
+        /// decimal conversion
+        /// </summary>
+        /// <param name="number"></param>
+        /// <param name="notation"></param>
+        /// <returns></returns>
+        public static long ToDec(string number, int notation)
+        {
+            if (number.Length >= 32)
+                throw new OverflowException();
+
+            if (notation < 2 || notation > 16)
+                throw new ArgumentException();
+
+            long result = 0;
+            for (int i = 0; i < number.Length; i++)
+            {
+                int tmp;
+                if (StringElement(number, i) < '0' || StringElement(number, i) > '9')
+                {
+                    tmp = char.ToUpper(StringElement(number, i)) - 'A' + 10;
+                    if (tmp >= notation)
+                        throw new ArgumentException();
+                }
+                else
+                {
+                    tmp = StringElement(number, i) - '0';
+                    if (tmp >= notation)
+                        throw new ArgumentException();
+                }
+                result = (long)Math.Pow(notation, number.Length - i - 1) * tmp + result;
+                
+            }
+            return result;
+        }
+        
+        /// <summary>
+        /// getting a string item
+        /// </summary>
+        /// <param name="number"></param>
+        /// <param name="index"></param>
+        /// <returns></returns>
+        private static char StringElement(string number, int index)
+        {
+            char ch = number[index];
+            return ch;
+
+        }
+    }
+}
---
+++ NET.S.2018.Levkovich.05/StringExtensionTest.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NUnit.Framework;
+using static ConvertToDecimal.StringExtension;
+
+namespace ConvertToDecimal.Test
+{
+    using System.Collections;
+
+    [TestFixture]
+    public class StringExtensionTest
+    {
+        [TestCase("0110111101100001100001010111111", 2, ExpectedResult = 934331071)]
+        public long ToDec_FromBin(string number, int notation)
+            => ToDec(number, notation);
+        [TestCase("01101111011001100001010111111", 2, ExpectedResult = 233620159)]
+        public long ToDec_FronBin02(string number, int notation)
+        {
+            return ToDec(number, notation);
+        }
+        [TestCase("11101101111011001100001010", 2, ExpectedResult = 62370570)]
+        public long ToDec_FromBin03(string number, int notation)
+        {
+            return ToDec(number, notation);
+        }
+        [TestCase("1AeF101", 16, ExpectedResult = 28242177)]
+        public long ToDec_FromSixteen(string number, int notation)
+        {
+            return ToDec(number, notation);
+        }
+        [TestCase("1ACB67", 16, ExpectedResult = 1756007)]
+        public long ToDec_FromSixteen02(string number, int notation)
+        {
+            return ToDec(number, notation);
+        }
+        [TestCase("764241", 8, ExpectedResult = 256161)]
+        public long ToDec_fromEight(string number, int notation)
+        {
+            return ToDec(number, notation);
+        }
+        [TestCase("10", 5, ExpectedResult = 5)]
+        public long ToDec_FromFifth(string number, int notation)
+        {
+            return ToDec(number, notation);
+        }
+        [TestCase("1AeF101", 2)]
+        public void ToDec_SmalBase(string number, int notation) 
+            => Assert.Throws<ArgumentException>(() => ToDec(number, notation));
+
+        [TestCase("1AeF101", 2)]
+        public void ToDec_SmalBase02(string number, int notation)
+            => Assert.Throws<ArgumentException>(() => ToDec(number, notation));
+
+        [TestCase("11111111111111111111111111111111", 2)]
+        public void ToDec_ToBigNumber(string number, int notation)
+           => Assert.Throws<OverflowException>(() => ToDec(number, notation));
+
+        [TestCase("764241", 20)]
+        public void ToDec_BigBase(string number, int notation)
+     => Assert.Throws<ArgumentException>(() => ToDec(number, notation));
+
+    }
+}
+
