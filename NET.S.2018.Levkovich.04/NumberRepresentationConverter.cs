20fe9fbf4c7c75c1539303d596b33a9246550c3a
Anastacia Levkovich <levkovich.astya@gmail.com>
2018-07-09 23:36:04 +0300

    refactoring

--- NET.S.2018.Levkovich.04/NumberRepresentationConverter.cs
+++ NET.S.2018.Levkovich.04/NumberRepresentationConverter.cs
@@ -6,99 +6,88 @@ namespace NumberRepresentationConverter
     /// <summary>
     /// Public class, that represent number in binary notation. 
     /// </summary>
-<<<<<<< HEAD
-
     public static class NumberRepresentationConverter
-    {
-        #region Constant
+     {
+         #region Constant
+            /// <summary>
+            /// Bit number constant.
+            /// </summary>
+            private const int BITS_IN_BYTE = 8;
 
-=======
-    public static class NumberRepresentationConverter
-    {
->>>>>>> bb6b9b95004721eead233a82bd4f028c5fc7f3b7
-        /// <summary>
-        /// Bit number constant.
-        /// </summary>
-        private const int BITS_IN_BYTE = 8;
-<<<<<<< HEAD
+            #endregion
 
-        #endregion
-      
-=======
->>>>>>> bb6b9b95004721eead233a82bd4f028c5fc7f3b7
-        #region Public methods
-        /// <summary>
-        /// Convert double to string IEEE 754 format.
-        /// </summary>
-        /// <param name="number"> Value to convert. </param>
-        /// <returns> String representation in IEEE 754 format. </returns>
-        public static string DoubleToBinaryString(this double number)
-        {
-            var tmp = new DoubleToLongStruct(number);
-            var tmp2 = tmp.Long64bits;
-            var result = new char[64];
-            for (var i = BITS_IN_BYTE * 8 - 1; i >= 0; i--)
+
+         #region Public methods
+
+            /// <summary>
+            /// Convert double to string IEEE 754 format.
+            /// </summary>
+            /// <param name="number"> Value to convert. </param>
+            /// <returns> String representation in IEEE 754 format. </returns>
+            public static string DoubleToBinaryString(this double number)
             {
-                result[i] = (tmp2 & 1) == 0 ? '0' : '1';
-                tmp2 >>= 1;
+                var tmp = new DoubleToLongStruct(number);
+                var tmp2 = tmp.Long64bits;
+                var result = new char[64];
+                for (var i = BITS_IN_BYTE * 8 - 1; i >= 0; i--)
+                {
+                    result[i] = (tmp2 & 1) == 0 ? '0' : '1';
+                    tmp2 >>= 1;
+                }
+
+                var results = new string(result);
+                return results;
             }
 
-            var results = new string(result);
-            return results;
-        }
-        #endregion
+            #endregion
 
-<<<<<<< HEAD
-        #region Struct
-=======
-        #region Struc
-        /// <summary>
-        /// Double and long represent in one field.
-        /// </summary>
->>>>>>> bb6b9b95004721eead233a82bd4f028c5fc7f3b7
-        [StructLayout(LayoutKind.Explicit)]
-        private struct DoubleToLongStruct
-        {
-            /// <summary>
-            /// Long representation.
-            /// </summary>
-            [FieldOffset(0)]
-            private readonly long long64bits;
-<<<<<<< HEAD
-            
-=======
+
+         #region Struct
 
             /// <summary>
-            /// Double representation.
+            /// Double and long represent in one field.
             /// </summary>
->>>>>>> bb6b9b95004721eead233a82bd4f028c5fc7f3b7
-            [FieldOffset(0)]
-            private double double64bits;
-
-            /// <summary>
-            /// Initializes struct.
-            /// </summary>
-            /// <param name="number"> Double decimal number. </param>
-            public DoubleToLongStruct(double number) : this()
+            [StructLayout(LayoutKind.Explicit)]
+            private struct DoubleToLongStruct
             {
-                this.Double64bits = number;
+                /// <summary>
+                /// Long representation.
+                /// </summary>
+                [FieldOffset(0)]
+                private readonly long long64bits;
+
+                /// <summary>
+                /// Double representation.
+                /// </summary>
+                [FieldOffset(0)]
+                private double double64bits;
+
+                /// <summary>
+                /// Initializes struct.
+                /// </summary>
+                /// <param name="number"> Double decimal number. </param>
+                public DoubleToLongStruct(double number)
+                    : this()
+                {
+                    this.Double64bits = number;
+                }
+
+                /// <summary>
+                /// Get long64bits 
+                /// </summary>
+                public long Long64bits => this.long64bits;
+
+                /// <summary>
+                /// Get and set Double64bits 
+                /// </summary>
+                public double Double64bits
+                {
+                    get => this.double64bits;
+                    set => this.double64bits = value;
+                }
             }
 
-            /// <summary>
-            /// Get long64bits 
-            /// </summary>
-            public long Long64bits => this.long64bits;
-
-            /// <summary>
-            /// Get and set Double64bits 
-            /// </summary>
-            public double Double64bits
-            {
-                get => this.double64bits;
-                set => this.double64bits = value;
-            }
-        }
-
-        #endregion
+            #endregion
     }
 }
+
