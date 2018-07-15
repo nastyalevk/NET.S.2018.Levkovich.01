3875c3347007f292a0d25a73c8b4cfcbfd7879b3
Anastacia Levkovich <levkovich.astya@gmail.com>
2018-07-15 23:14:06 +0300

    Jagged array sort

---
+++ NET.S.2018.Levkovich.09/JaggedArray.cs
@@ -0,0 +1,150 @@
+﻿using System;
+
+namespace Logic
+{
+    using System.Diagnostics.CodeAnalysis;
+
+    /// <summary>
+    /// Class with sorting methods
+    /// </summary>
+    public static class JaggedArray
+    {
+        /// <summary>
+        /// The bubble sort to less.
+        /// </summary>
+        /// <param name="jaggedArray">
+        /// The jagged array.
+        /// </param>
+        /// <param name="howSort">
+        /// The how sort.
+        /// </param>
+        /// <returns>
+        /// The <see cref="int[][]"/>.
+        /// </returns>
+        /// <exception cref="ArgumentNullException">
+        /// </exception>
+        /// <exception cref="ArgumentException">
+        /// </exception>
+        public static int[][] BubbleToLess(this int[][] jaggedArray, string howSort)
+        {
+            if (jaggedArray == null)
+                throw new ArgumentNullException();
+            for (int i = 0; i < jaggedArray.GetLength(0) - 1; i++)
+            {
+                for (int j = 1; j <= jaggedArray.GetLength(0) - 1; j++)
+                {
+                    switch (howSort)
+                    {
+                        case "SumToMore":
+                            {
+                                if (SumToMore(jaggedArray[i]) < SumToMore(jaggedArray[j]))
+                                    Swap(ref jaggedArray[i], ref jaggedArray[j]);
+                                break;
+                            }
+                        case "MaxEl":
+                            {
+                                if (MaxEl(jaggedArray[i]) < MaxEl(jaggedArray[j]))
+                                    Swap(ref jaggedArray[i], ref jaggedArray[j]);
+                                break;
+                            }
+                        case "MinEl":
+                            {
+                                if (MinEl(jaggedArray[i]) < MinEl(jaggedArray[j]))
+                                    Swap(ref jaggedArray[i], ref jaggedArray[j]);
+                                break;
+                            }
+                        default:
+                            throw new ArgumentException();
+                    }
+                }
+            }
+            return jaggedArray;
+        }
+
+        /// <summary>
+        /// The bubble sort to more.
+        /// </summary>
+        /// <param name="jaggedArray">
+        /// The jagged array.
+        /// </param>
+        /// <param name="howSort">
+        /// The how sort.
+        /// </param>
+        /// <returns>
+        /// The <see cref="int[][]"/>.
+        /// </returns>
+        /// <exception cref="ArgumentNullException">
+        /// </exception>
+        /// <exception cref="ArgumentException">
+        /// </exception>
+        public static int[][] BubbleToMore(this int[][] jaggedArray, string howSort)
+        {
+            if (jaggedArray == null)
+                throw new ArgumentNullException();
+            for (int i = 0; i < jaggedArray.GetLength(0) - 1; i++)
+            {
+                for (int j = 1; j <= jaggedArray.GetLength(0) - 1; j++)
+                {
+                    switch (howSort)
+                    {
+                        case "SumToMore":
+                            {
+                                if (SumToMore(jaggedArray[i]) > SumToMore(jaggedArray[j]))
+                                    Swap(ref jaggedArray[i], ref jaggedArray[j]);
+                                break;
+                            }
+                        case "MaxEl":
+                            {
+                                if (MaxEl(jaggedArray[i]) > MaxEl(jaggedArray[j]))
+                                    Swap(ref jaggedArray[i], ref jaggedArray[j]);
+                                break;
+                            }
+                        case "MinEl":
+                            {
+                                if (MinEl(jaggedArray[i]) > MinEl(jaggedArray[j]))
+                                    Swap(ref jaggedArray[i], ref jaggedArray[j]);
+                                break;
+                            }
+                        default:
+                            throw new ArgumentException();
+                    }
+                }
+            }
+            return jaggedArray;
+        }
+
+        private static void Swap(ref int[] left, ref int[] rigth)
+        {
+            int[] tmp = left;
+            left = rigth;
+            rigth = tmp;
+        }
+        private static int SumToMore(int[] item)
+        {
+            int sum = 0;
+            for (int i = 0; i < item.Length; i++)
+            { sum += item[i]; }
+            return sum;
+        }
+        private static int MaxEl(int[] item)
+        {
+            int max = item[0];
+            for (int i = 1; i < item.Length; ++i)
+            {
+                if (item[i] > max)
+                    max = item[i];
+            }
+            return max;
+        }
+        private static int MinEl(int[] item)
+        {
+            int min = item[0];
+            for (int i = 1; i < item.Length; ++i)
+            {
+                if (item[i] < min)
+                    min = item[i];
+            }
+            return min;
+        }
+    }
+}
---
+++ NET.S.2018.Levkovich.09/JaggedArrayTest.cs
@@ -0,0 +1,104 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Logic.Test
+{
+    using NUnit.Framework;
+
+    [TestFixture]
+    public class JaggedArrayTest
+    {
+        [Test]
+        public void BubbleToMore_Sum()
+        {
+            int[][] actual =
+                { new int[] { 1, 9, 50, 7, 9 }, new int[] { 112, 22, 77, 55 }, new int[] { 37, 41, 6, 94 } };
+            ;
+
+            int[][] expected =
+                { new int[] { 1, 9, 50, 7, 9 }, new int[] { 37, 41, 6, 94 }, new int[] { 112, 22, 77, 55 } };
+
+            actual.BubbleToMore("SumToMore");
+
+            CollectionAssert.AreEqual(actual, expected);
+        }
+
+        [Test]
+        public void BubbleToMore_MaxEl()
+        {
+            int[][] actual =
+                { new int[] { 1, 9, 50, 7, 9 }, new int[] { 112, 22, 77, 55 }, new int[] { 37, 41, 6, 94 } };
+            ;
+
+            int[][] expected =
+                { new int[] { 1, 9, 50, 7, 9 }, new int[] { 37, 41, 6, 94 }, new int[] { 112, 22, 77, 55 } };
+
+            actual.BubbleToMore("MaxEl");
+
+            CollectionAssert.AreEqual(actual, expected);
+        }
+
+        [Test]
+        public void BubbleToMore_MinEl()
+        {
+            int[][] actual =
+                { new int[] { 1, 9, 50, 7, 9 }, new int[] { 112, 22, 77, 55 }, new int[] { 37, 41, 6, 94 } };
+            ;
+
+            int[][] expected =
+                { new int[] { 1, 9, 50, 7, 9 }, new int[] { 37, 41, 6, 94 }, new int[] { 112, 22, 77, 55 } };
+
+            actual.BubbleToMore("MinEl");
+
+            CollectionAssert.AreEqual(actual, expected);
+        }
+
+        [Test]
+        public void BubbleToLess_Sum()
+        {
+            int[][] actual =
+                { new int[] { 1, 9, 50, 7, 9 }, new int[] { 112, 22, 77, 55 }, new int[] { 37, 41, 6, 94 } };
+            ;
+
+            int[][] expected =
+                { new int[] { 112, 22, 77, 55 }, new int[] { 37, 41, 6, 94 }, new int[] { 1, 9, 50, 7, 9 } };
+
+            actual.BubbleToLess("SumToMore");
+
+            CollectionAssert.AreEqual(actual, expected);
+        }
+
+        [Test]
+        public void BubbleToLess_MaxEl()
+        {
+            int[][] actual =
+                { new int[] { 1, 9, 50, 7, 9 }, new int[] { 112, 22, 77, 55 }, new int[] { 37, 41, 6, 94 } };
+            ;
+
+            int[][] expected =
+                { new int[] { 112, 22, 77, 55 }, new int[] { 37, 41, 6, 94 }, new int[] { 1, 9, 50, 7, 9 } };
+
+            actual.BubbleToLess("MaxEl");
+
+            CollectionAssert.AreEqual(actual, expected);
+        }
+
+        [Test]
+        public void BubbleToLess_MinEl()
+        {
+            int[][] actual =
+                { new int[] { 1, 9, 50, 7, 9 }, new int[] { 112, 22, 77, 55 }, new int[] { 37, 41, 6, 94 } };
+            ;
+
+            int[][] expected =
+                { new int[] { 112, 22, 77, 55 }, new int[] { 37, 41, 6, 94 }, new int[] { 1, 9, 50, 7, 9 } };
+
+            actual.BubbleToLess("MinEl");
+
+            CollectionAssert.AreEqual(actual, expected);
+        }
+    }
+}
