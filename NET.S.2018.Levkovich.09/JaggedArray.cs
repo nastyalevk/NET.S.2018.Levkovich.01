91fb12194e5441a445b5d4d09e6edb05dfb1a580
Anastacia Levkovich <levkovich.astya@gmail.com>
2018-07-18 18:22:23 +0300

    jagged

--- NET.S.2018.Levkovich.09/JaggedArray.cs
+++ NET.S.2018.Levkovich.09/JaggedArray.cs
@@ -7,110 +7,22 @@ namespace Logic
     /// <summary>
     /// Class with sorting methods
     /// </summary>
-    public static class JaggedArray
+    public interface ICompare
     {
-        /// <summary>
-        /// The bubble sort to less.
-        /// </summary>
-        /// <param name="jaggedArray">
-        /// The jagged array.
-        /// </param>
-        /// <param name="howSort">
-        /// The how sort.
-        /// </param>
-        /// <returns>
-        /// The <see cref="int[][]"/>.
-        /// </returns>
-        /// <exception cref="ArgumentNullException">
-        /// </exception>
-        /// <exception cref="ArgumentException">
-        /// </exception>
-        public static int[][] BubbleToLess(this int[][] jaggedArray, string howSort)
+        int Compare(int[] left, int[] right);
+    }
+    public static class Array
+    {
+        public static void BubbleSort(this int[][] jaggedArray, ICompare howToSort)
         {
-            if (jaggedArray == null)
-                throw new ArgumentNullException();
             for (int i = 0; i < jaggedArray.GetLength(0) - 1; i++)
             {
                 for (int j = 1; j <= jaggedArray.GetLength(0) - 1; j++)
                 {
-                    switch (howSort)
-                    {
-                        case "SumToMore":
-                            {
-                                if (SumToMore(jaggedArray[i]) < SumToMore(jaggedArray[j]))
-                                    Swap(ref jaggedArray[i], ref jaggedArray[j]);
-                                break;
-                            }
-                        case "MaxEl":
-                            {
-                                if (MaxEl(jaggedArray[i]) < MaxEl(jaggedArray[j]))
-                                    Swap(ref jaggedArray[i], ref jaggedArray[j]);
-                                break;
-                            }
-                        case "MinEl":
-                            {
-                                if (MinEl(jaggedArray[i]) < MinEl(jaggedArray[j]))
-                                    Swap(ref jaggedArray[i], ref jaggedArray[j]);
-                                break;
-                            }
-                        default:
-                            throw new ArgumentException();
-                    }
+                    if (howToSort.Compare(jaggedArray[i], jaggedArray[j]) == -1)
+                        Swap(ref jaggedArray[i], ref jaggedArray[j]);
                 }
             }
-            return jaggedArray;
-        }
-
-        /// <summary>
-        /// The bubble sort to more.
-        /// </summary>
-        /// <param name="jaggedArray">
-        /// The jagged array.
-        /// </param>
-        /// <param name="howSort">
-        /// The how sort.
-        /// </param>
-        /// <returns>
-        /// The <see cref="int[][]"/>.
-        /// </returns>
-        /// <exception cref="ArgumentNullException">
-        /// </exception>
-        /// <exception cref="ArgumentException">
-        /// </exception>
-        public static int[][] BubbleToMore(this int[][] jaggedArray, string howSort)
-        {
-            if (jaggedArray == null)
-                throw new ArgumentNullException();
-            for (int i = 0; i < jaggedArray.GetLength(0) - 1; i++)
-            {
-                for (int j = 1; j <= jaggedArray.GetLength(0) - 1; j++)
-                {
-                    switch (howSort)
-                    {
-                        case "SumToMore":
-                            {
-                                if (SumToMore(jaggedArray[i]) > SumToMore(jaggedArray[j]))
-                                    Swap(ref jaggedArray[i], ref jaggedArray[j]);
-                                break;
-                            }
-                        case "MaxEl":
-                            {
-                                if (MaxEl(jaggedArray[i]) > MaxEl(jaggedArray[j]))
-                                    Swap(ref jaggedArray[i], ref jaggedArray[j]);
-                                break;
-                            }
-                        case "MinEl":
-                            {
-                                if (MinEl(jaggedArray[i]) > MinEl(jaggedArray[j]))
-                                    Swap(ref jaggedArray[i], ref jaggedArray[j]);
-                                break;
-                            }
-                        default:
-                            throw new ArgumentException();
-                    }
-                }
-            }
-            return jaggedArray;
         }
 
         private static void Swap(ref int[] left, ref int[] rigth)
@@ -119,32 +31,7 @@ namespace Logic
             left = rigth;
             rigth = tmp;
         }
-        private static int SumToMore(int[] item)
-        {
-            int sum = 0;
-            for (int i = 0; i < item.Length; i++)
-            { sum += item[i]; }
-            return sum;
-        }
-        private static int MaxEl(int[] item)
-        {
-            int max = item[0];
-            for (int i = 1; i < item.Length; ++i)
-            {
-                if (item[i] > max)
-                    max = item[i];
-            }
-            return max;
-        }
-        private static int MinEl(int[] item)
-        {
-            int min = item[0];
-            for (int i = 1; i < item.Length; ++i)
-            {
-                if (item[i] < min)
-                    min = item[i];
-            }
-            return min;
-        }
     }
+
+
 }
--- NET.S.2018.Levkovich.09/JaggedArrayTest.cs
+++ NET.S.2018.Levkovich.09/JaggedArrayTest.cs
@@ -21,7 +21,7 @@ namespace Logic.Test
             int[][] expected =
                 { new int[] { 1, 9, 50, 7, 9 }, new int[] { 37, 41, 6, 94 }, new int[] { 112, 22, 77, 55 } };
 
-            actual.BubbleToMore("SumToMore");
+            Array.BubbleSort(actual, new ToMoreBySumm());
 
             CollectionAssert.AreEqual(actual, expected);
         }
@@ -36,7 +36,7 @@ namespace Logic.Test
             int[][] expected =
                 { new int[] { 1, 9, 50, 7, 9 }, new int[] { 37, 41, 6, 94 }, new int[] { 112, 22, 77, 55 } };
 
-            actual.BubbleToMore("MaxEl");
+            Array.BubbleSort(actual, new ToMoreByMaxEl());
 
             CollectionAssert.AreEqual(actual, expected);
         }
@@ -51,7 +51,7 @@ namespace Logic.Test
             int[][] expected =
                 { new int[] { 1, 9, 50, 7, 9 }, new int[] { 37, 41, 6, 94 }, new int[] { 112, 22, 77, 55 } };
 
-            actual.BubbleToMore("MinEl");
+            Array.BubbleSort(actual, new ToMoreByMinEl());
 
             CollectionAssert.AreEqual(actual, expected);
         }
@@ -66,7 +66,7 @@ namespace Logic.Test
             int[][] expected =
                 { new int[] { 112, 22, 77, 55 }, new int[] { 37, 41, 6, 94 }, new int[] { 1, 9, 50, 7, 9 } };
 
-            actual.BubbleToLess("SumToMore");
+            Array.BubbleSort(actual, new ToLessBySumm());
 
             CollectionAssert.AreEqual(actual, expected);
         }
@@ -81,7 +81,7 @@ namespace Logic.Test
             int[][] expected =
                 { new int[] { 112, 22, 77, 55 }, new int[] { 37, 41, 6, 94 }, new int[] { 1, 9, 50, 7, 9 } };
 
-            actual.BubbleToLess("MaxEl");
+            Array.BubbleSort(actual, new ToLessByMaxEl());
 
             CollectionAssert.AreEqual(actual, expected);
         }
@@ -96,9 +96,110 @@ namespace Logic.Test
             int[][] expected =
                 { new int[] { 112, 22, 77, 55 }, new int[] { 37, 41, 6, 94 }, new int[] { 1, 9, 50, 7, 9 } };
 
-            actual.BubbleToLess("MinEl");
+            Array.BubbleSort(actual, new ToLessByMinEl());
 
             CollectionAssert.AreEqual(actual, expected);
         }
     }
+
+    public class ToMoreBySumm : ICompare
+    {
+        public int Compare(int[] left, int[] right)
+        {
+            if (SortingCriteria.Sum(left) > SortingCriteria.Sum(right))
+                return -1;
+            if (SortingCriteria.Sum(left) < SortingCriteria.Sum(right))
+                return 1;
+            return 0;
+        }
+    }
+    public class ToMoreByMaxEl : ICompare
+    {
+        public int Compare(int[] left, int[] right)
+        {
+            if (SortingCriteria.MaxEl(left) > SortingCriteria.MaxEl(right))
+                return -1;
+            if (SortingCriteria.MaxEl(left) < SortingCriteria.MaxEl(right))
+                return 1;
+            return 0;
+        }
+    }
+    public class ToMoreByMinEl : ICompare
+    {
+        public int Compare(int[] left, int[] right)
+        {
+            if (SortingCriteria.MinEl(left) > SortingCriteria.MinEl(right))
+                return -1;
+            if (SortingCriteria.MinEl(left) < SortingCriteria.MinEl(right))
+                return 1;
+            return 0;
+        }
+    }
+    public class ToLessBySumm : ICompare
+    {
+        public int Compare(int[] left, int[] right)
+        {
+            if (SortingCriteria.Sum(left) < SortingCriteria.Sum(right))
+                return -1;
+            if (SortingCriteria.Sum(left) > SortingCriteria.Sum(right))
+                return 1;
+            return 0;
+        }
+    }
+    public class ToLessByMaxEl : ICompare
+    {
+        public int Compare(int[] left, int[] right)
+        {
+            if (SortingCriteria.MaxEl(left) < SortingCriteria.MaxEl(right))
+                return -1;
+            if (SortingCriteria.MaxEl(left) > SortingCriteria.MaxEl(right))
+                return 1;
+            return 0;
+        }
+    }
+    public class ToLessByMinEl : ICompare
+    {
+        public int Compare(int[] left, int[] right)
+        {
+            if (SortingCriteria.MinEl(left) < SortingCriteria.MinEl(right))
+                return -1;
+            if (SortingCriteria.MinEl(left) > SortingCriteria.MinEl(right))
+                return 1;
+            return 0;
+        }
+    }
+
+    public static class SortingCriteria
+    {
+        public static int Sum(int[] item)
+        {
+
+            int sum = 0;
+            for (int i = 0; i < item.Length; i++)
+            { sum += item[i]; }
+            return sum;
+        }
+        public static int MaxEl(int[] item)
+        {
+            int max = item[0];
+            for (int i = 1; i < item.Length; ++i)
+            {
+                if (item[i] > max)
+                    max = item[i];
+            }
+            return max;
+        }
+        public static int MinEl(int[] item)
+        {
+
+            int min = item[0];
+            for (int i = 1; i < item.Length; ++i)
+            {
+                if (item[i] < min)
+                    min = item[i];
+            }
+            return min;
+        }
+    }
+
 }
